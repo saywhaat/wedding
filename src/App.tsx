4204086d576135fa824b7887b20d9f7b9e100481
Dmitrii Kuznetcov <kuznecov.da@medindex.ru>
2021-08-18 13:55:39 +0300

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  useParams,
+  useParams
 } from "react-router-dom";
 import { guests } from "./data_encrypted.json";
 import we from "./we.png";
+import weddingSrc from "./wedding.png";
 import "wired-elements";
 import history from "./history";
 import { SHA256, AES, enc } from "crypto-js";
@@ -19,61 +20,44 @@ declare global {
   }
 }
 
+const DEFAULT_GUEST = {
+  name: "Привет друг! Если ты получал персональную ссылку, то",
+  single: true
+};
+
 function Home() {
+  const [showHint, setShowHint] = React.useState(false);
   let { secret } = useParams<{ secret: string }>();
-  const encryptedGuest = guests.find(
-    (d) => d.id === SHA256(secret).toString()
-  )!;
-  const guestData = JSON.parse(
-    AES.decrypt(encryptedGuest.data, secret).toString(enc.Utf8)
-  );
-  const guest = {
-    name: guestData.name,
-  };
+  const encryptedGuest = guests.find(d => d.id === SHA256(secret).toString());
+  const guest = encryptedGuest
+    ? JSON.parse(AES.decrypt(encryptedGuest.data, secret).toString(enc.Utf8))
+    : DEFAULT_GUEST;
 
   return (
-    <div className="container max-w-screen-md	 mx-auto">
+    <div className="container max-w-screen-md	 mx-auto py-32 overflow-hidden">
       <div className="flex flex-col items-center text-center">
-        <div className="text-5xl mt-48">{guest.name}</div>
-        <div className="text-2xl max-w-xs">
-          Приглашаем вас на свадьбу 17 сентября
+        <div className="text-5xl">{guest.name}</div>
+        <div className="text-2xl max-w-xs mt-16">
+          Приглашаем {guest.single ? "тебя" : "вас"} на свадьбу 17 сентября
+          (пятница)
         </div>
         <div className="text-lg mt-8">Интересно к кому?</div>
-        <img src={we} className="w-60"/>
-        <div className="text-lg flex gap-4 max-w-xs">
+        <img src={we} className="w-60" style={{ mixBlendMode: "multiply" }} />
+        <div className="text-lg flex gap-4 max-w-xs mt-8">
           <div>Диме Кузнецову</div>
           <div>Даше Елисеевой</div>
         </div>
-        <div className="text-2xl mt-12">Придете?</div>
-        <div className="flex mt-6">
-          <div>
-            <wired-button elevation="2">
-              <div className="text-lg pt-2 px-4">Конечно!</div>
-            </wired-button>
-          </div>
-          <div className="ml-8">
-            <wired-button elevation="2">
-              <div className="text-lg pt-2 px-4">Нет</div>
-            </wired-button>
-          </div>
-        </div>
-        <iframe
-          className="w-full"
-          src="https://docs.google.com/forms/d/e/1FAIpQLSdZoEBRjE2LkWCJQ-GJ8NKpuI-QhhDQeJ5ihSlbG4EDCYfM2A/viewform?embedded=true"
-          frameBorder="0"
-          marginHeight={0}
-          marginWidth={0}
-          height={820}
-        >
-          Loading…
-        </iframe>
       </div>
 
-      <div className="flex flex-col items-center">
-        <div className="text-5xl mt-24">История отношений</div>
+      <div className="flex flex-col items-center mt-24">
+        <div className="text-5xl">История отношений</div>
         {history.map((d, i) => (
-          <div className={`flex gap-8 mt-8 ${i % 2 ? "flex-row-reverse" : ""}`}>
-            <img src={d.image} className="w-2/5"/>
+          <div
+            className={`flex flex-col lg:flex-row gap-8 mt-8 ${
+              i % 2 ? "lg:flex-row-reverse" : ""
+            }`}
+          >
+            <img src={d.image} className="lg:w-2/5" />
             <div className="flex flex-col justify-center">
               <div>{d.title}</div>
               <div className="mt-4">{d.text}</div>
@@ -81,6 +65,31 @@ function Home() {
           </div>
         ))}
       </div>
+
+      <div className="flex flex-col mt-24">
+        <div className="text-5xl">
+          Где собираемся? Куда едем? Как одеваться? Что дарить
+          <span
+            style={{
+              color: "#5DB4EC",
+              cursor: "pointer",
+              textDecoration: "underline"
+            }}
+            onMouseEnter={() => setShowHint(true)}
+            onMouseLeave={() => setShowHint(false)}
+          >
+            ?
+          </span>
+        </div>
+        <div className="text-lg mt-8">
+          Вся информация очень скоро появится на этом сайте. Следите за
+          обновлениями!
+        </div>
+      </div>
+
+      {showHint ? (
+        <div className="fixed bottom-0 left-0 text-6xl">💰</div>
+      ) : null}
     </div>
   );
 }
@@ -89,7 +98,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/:secret">
+        <Route path={["/:secret", ""]}>
           <Home />
         </Route>
       </Switch>
