22a22c0e5f578209ad1a6c65265223f1018fef98
Dmitrii Kuznetcov <kuznecov.da@medindex.ru>
2021-08-18 16:21:56 +0300

    content

--- src/encrypt.js
+++ src/encrypt.js
@@ -3,13 +3,10 @@ const fs = require("fs");
 let json = fs.readFileSync("data.json");
 const { guests } = JSON.parse(json);
 const encryptedData = {
-  guests: guests.map((d) => ({
+  guests: guests.map(d => ({
     id: CryptoJS.SHA256(d.secret).toString(),
-    data: CryptoJS.AES.encrypt(
-      JSON.stringify({ name: d.name }),
-      d.secret
-    ).toString(),
-  })),
+    data: CryptoJS.AES.encrypt(JSON.stringify(d.data), d.secret).toString()
+  }))
 };
 json = JSON.stringify(encryptedData, null, 2);
 fs.writeFileSync("data_encrypted.json", json);
